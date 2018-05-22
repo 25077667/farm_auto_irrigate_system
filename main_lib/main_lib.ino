8c7b1911a851f32cddbb5572eae2a39def814696
25077667 <zxc25077667@gmail.com>
2018-05-22 09:47:56 +0800

    2

--- main_lib/main_lib.ino
+++ main_lib/main_lib.ino
@@ -1,1 +1,26 @@
-1
+#include <DS1302.h>
+
+#define RTC_RST 10
+#define RTC_DAT 9
+#define RTC_CLOCK 8
+
+#define relay_pin 2
+
+DS1302 rtc(10, 9, 8);
+
+/*
+void set_time(){
+   rtc.setDOW(FRIDAY);        // 設定週幾，如FRIDAY
+  rtc.setTime(21, 35, 0);     // 設定時間 時，分，秒 (24hr format)
+  rtc.setDate(5, 8, 2016);   // 設定日期 日，月，年
+}
+*/
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("hello");
+  delay(1000);
+}
