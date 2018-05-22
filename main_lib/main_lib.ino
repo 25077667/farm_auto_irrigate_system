b53f5f1900d24c4e0520030cd1a54dee9e4bb86c
25077667 <zxc25077667@gmail.com>
2018-05-22 11:04:07 +0800

    4

--- main_lib/main_lib.ino
+++ main_lib/main_lib.ino
@@ -4,6 +4,11 @@
 #define RTC_DAT 9
 #define RTC_CLOCK 8
 
+String sth = ""; //接受時間回傳值
+String hh = "";
+String mm = "";
+String ss = "";
+
 #define relay_pin 2
 
 DS1302 rtc(10, 9, 8);
@@ -14,6 +19,10 @@ void set_time(){
   rtc.setDate(5, 8, 2016);   // 設定日期 日，月，年
 }
 
+void water(){
+  
+}
+
 void setup() {
   Serial.begin(9600);
   //set_time();  //第一次傳就好
@@ -21,6 +30,13 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("hello");
-  delay(1000);
+  //Serial.println("hello");
+  //delay(1000);
+  sth = rtc.getTimeStr();
+  hh = sth.substring(0,2);
+  //mm = sth.substring(3,5);
+  //ss = sth.substring(6,8);
+  int hhh = hh.toInt();
+  if (hhh == 6 || hhh == 19)
+    water();
 }
