573da5363140d42e19218d09b9e2bd99e204b03b
25077667 <zxc25077667@gmail.com>
2018-05-22 10:06:53 +0800

    3

--- main_lib/main_lib.ino
+++ main_lib/main_lib.ino
@@ -8,16 +8,16 @@
 
 DS1302 rtc(10, 9, 8);
 
-/*
 void set_time(){
-   rtc.setDOW(FRIDAY);        // 設定週幾，如FRIDAY
+  rtc.setDOW(FRIDAY);        // 設定週幾，如FRIDAY
   rtc.setTime(21, 35, 0);     // 設定時間 時，分，秒 (24hr format)
   rtc.setDate(5, 8, 2016);   // 設定日期 日，月，年
 }
-*/
 
 void setup() {
   Serial.begin(9600);
+  //set_time();  //第一次傳就好
+  
 }
 
 void loop() {
