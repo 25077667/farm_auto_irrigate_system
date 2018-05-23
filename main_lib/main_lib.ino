67310c0d826a8a424d30e1a803996bd4059c55d0
25077667 <zxc25077667@gmail.com>
2018-05-23 11:54:19 +0800

    4.6

--- main_lib/main_lib.ino
+++ main_lib/main_lib.ino
@@ -14,9 +14,11 @@ String ss = "";
 DS1302 rtc(10, 9, 8);
 
 void set_time(){
+  rtc.halt(false);
+  rtc.writeProtect(false);
   rtc.setDOW(FRIDAY);        // 設定週幾，如FRIDAY
-  rtc.setTime(21, 35, 0);     // 設定時間 時，分，秒 (24hr format)
-  rtc.setDate(5, 8, 2016);   // 設定日期 日，月，年
+  rtc.setTime(11, 47, 0);     // 設定時間 時，分，秒 (24hr format)
+  rtc.setDate(5, 23, 2016);   // 設定日期 日，月，年
 }
 
 void water(){
@@ -39,14 +41,17 @@ void auto_pull(int mode){
 
 void setup() {
   Serial.begin(9600);
-  //set_time();  //第一次傳就好
+  set_time();  //第一次傳就好
   pinMode(relay_pin,OUTPUT);
+  
 
 }
 
 void loop() {
   //Serial.println("hello");
-  delay(600000);
+  //delay(600000);
+  Serial.println(rtc.getTimeStr());
+  delay(1000);
   sth = rtc.getTimeStr();
   hh = sth.substring(0,2);
   //mm = sth.substring(3,5);
