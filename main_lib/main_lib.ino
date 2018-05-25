72bb1908cf82d85087a1be4d813b0be921c3bb53
25077667 <zxc25077667@gmail.com>
2018-05-25 21:31:38 +0800

    successful

--- main_lib/main_lib.ino
+++ main_lib/main_lib.ino
@@ -32,6 +32,8 @@ void show_time(){
   }  
 }
 
+int a;
+
 void water(){
   // average output water in balence line is 69.6 ml/s
   //evaluate 8 plants in our project
@@ -67,24 +69,31 @@ void setup() {
   rtc.halt(false);
   Serial.begin(9600);
   BT.begin(9600);
-  set_time();  //第一次傳就好
+  //set_time();  //第一次傳就好
   pinMode(relay_pin,OUTPUT);
 }
 
 void loop() {
   //Serial.println("hello");
   //delay(600000);
-  show_time();
+  //show_time();
+  char readin = '0';
   sth = rtc.getTimeStr();
   hh = sth.substring(0,2);   //mm = sth.substring(3,5);ss = sth.substring(6,8);
   int hhh = hh.toInt();
-  if ( hhh == 6 || hhh == 199)
+  if ( hhh == 6 || hhh == 19)
     water(); //註解1號
-  if ( hhh == 7 || hhh == 209)
+  if ( hhh == 7 || hhh == 20)
     water_flag = true; //註解二號
     
   // 早上6點會去澆水(呼叫water()函式)，然後water會把water_flag關掉，早上7點再把water_flag打開，直到19點符合註解1號
   // 因為只要是早上6點，就不會是早上7點，所以關了之後7點再開就不會撞到6點
   
-  check_bt_input();
+  if(BT.available()){
+    readin = BT.read();
+    Serial.write(readin);
+    Serial.write("\n");
+    digitalWrite(relay_pin, readin-'0');
+  }
+  //check_bt_input();
 }
