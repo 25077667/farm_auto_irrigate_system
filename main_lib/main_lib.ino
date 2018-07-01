ae0b304c3fc0cc62faa9128c5ddef630e09993bd
25077667 <zxc25077667@gmail.com>
2018-07-01 16:13:25 +0800

    revise the time revisng func. and clear the code

--- main_lib/main_lib.ino
+++ main_lib/main_lib.ino
@@ -1,7 +1,8 @@
 #include <SoftwareSerial.h>
 #include <DS1302.h>
-// 有些可悲bug沒辦法再loop裡面call check_bt_input，只能寫死在loop裡
 // 時間寫不上去的話，拔電池等5秒，重刷主機板再裝電池
+// DS1302真的很糟糕，平均每25min就會自己往前跳20秒，讓我沒事還要寫一個修正時間的 time_revising() 函式
+// 而且我不可能一直看他的log，不知道他時間到底何時會往前跳，只能很消極的每天挪回18分鐘，一段時間之後還是要手動重新修正時間
 #define RTC_RST 10
 #define RTC_DAT 9
 #define RTC_CLOCK 8
@@ -15,7 +16,6 @@ String sth = ""; //接受時間回傳值
 String hh = "";
 String mm = "";
 String ss = "";
-String message=""; //藍芽接收字串
 
 bool water_flag = true;  //調控澆水開關，true代表要澆水
 bool time_revise_flag = true;  //true 代表還沒調整時間
@@ -23,7 +23,7 @@ bool time_revise_flag = true;  //true 代表還沒調整時間
 void set_time(){
   rtc.writeProtect(false);
   rtc.setDOW(FRIDAY);        // 設定週幾，如FRIDAY
-  rtc.setTime(13,32, 0);     // 設定時間 時，分，秒 (24hr format)
+  rtc.setTime(13, 32, 0);     // 設定時間 時，分，秒 (24hr format)
   rtc.setDate(1, 7, 2018);   // 設定日期 日，月，年
 }
 void show_time(){
@@ -44,16 +44,8 @@ void water(){
   }
 }
 
-void check_bt_input(){
-  // 手機用藍芽控制是否直接出水，手機記得自己關水
-  if(BT.available()){
-      Serial.write(BT.read());
-      digitalWrite(relay_pin, BT.read()-'0');
-  }
-}
-
-void time_revising(String hh,String mm,String ss){
-  if(time_revise_flag == true && hh == "00" && ss=="mm" ){
+void time_revising(String hh,String mm ){
+  if(time_revise_flag == true && hh == "00" && mm=="18" ){
     rtc.setTime(0,0,0);
     time_revise_flag = false;
   }
@@ -73,16 +65,19 @@ void loop() {
   sth = rtc.getTimeStr();
   hh = sth.substring(0,2);   
   mm = sth.substring(3,5);
-  ss = sth.substring(6,8);
+  //ss = sth.substring(6,8);
   
   int hhh = hh.toInt();
   if ( hhh == 5 || hhh == 19)
     water(); //註解1號
-  if ( hhh == 6 || hhh == 20)
+  if ( hhh == 6 || hhh == 20){
+    time_revise_flag = true;
     water_flag = true; //註解二號
+  }
     
-  // 早上6點會去澆水(呼叫water()函式)，然後water會把water_flag關掉，早上7點再把water_flag打開，直到19點符合註解1號
-  // 因為只要是早上6點，就不會是早上7點，所以關了之後7點再開就不會撞到6點
+  // 早上5點會去澆水(呼叫water()函式)，然後water會把water_flag關掉，早上6點再把water_flag打開，直到19點符合註解1號
+  // 因為只要是早上5點，就不會是早上6點，所以關了之後6點再開就不會撞到5點
+  // 早上6點，把time_revise_flag打開，讓晚上00:18可以再用
   
   if(BT.available()){
     readin = BT.read();    
@@ -91,8 +86,5 @@ void loop() {
     digitalWrite(relay_pin, readin-'0');
   }
 
-  time_revising(hh, mm, ss);
-  if (hhh == 5)
-    time_revise_flag = true;
-  //check_bt_input();
+  time_revising(hh, mm);
 }
