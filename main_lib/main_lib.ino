a8e1357c1f142d4861f7ee0ce98963face7404d9
25077667 <zxc25077667@gmail.com>
2018-12-31 14:57:38 +0800

    enhance program

--- main_lib/main_lib.ino
+++ main_lib/main_lib.ino
@@ -1,15 +1,16 @@
 #include <DS3231.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
+#define watering_delay 185185
+#define morning 6
+#define night 16
+#define relay_pin 2
 
-// 希望永無bug!!!
-
-#define relay_pin 2   // relay_pin是給繼電器用的
 DS3231 Clock;
 bool h12;
 bool PM;
 bool Century=false;
-bool water_flag = true;  //調控澆水開關，true代表要澆水
+bool had_been_irrigated = false;
 SoftwareSerial BT(7, 6); //arduino RX/TX
 
 void set_time(){
@@ -17,10 +18,11 @@ void set_time(){
   Clock.setMonth(12);
   Clock.setDate(9);
   Clock.setDoW(0);
-  Clock.setHour(11);	//24小時制
+  Clock.setHour(11);	//the format is 24 hours
   Clock.setMinute(44);
   Clock.setSecond(0);
 }
+
 void show_time(){
       Serial.print(Clock.getYear(), DEC);
       Serial.print("-");
@@ -28,8 +30,7 @@ void show_time(){
       Serial.print("-");
       Serial.print(Clock.getDate(), DEC);
       Serial.print(" ");
-      int hh = Clock.getHour(h12,PM);
-      Serial.print(hh, DEC); //24-hr
+      Serial.print((int)Clock.getHour(h12,PM), DEC); //24-hr
       Serial.print(":");
       Serial.print(Clock.getMinute(), DEC);
       Serial.print(":");
@@ -38,41 +39,38 @@ void show_time(){
 }
 
 void water(){
-  // 每秒 18 ml
-  if ( water_flag == true ){
-    water_flag = false;
-    digitalWrite(relay_pin, 1);//繼電器啟動電磁閥
-    delay(185185); //185秒多
-    digitalWrite(relay_pin, 0);
+  if ( had_been_irrigated == false ){
+    had_been_irrigated = true;
+    digitalWrite(relay_pin, true);
+    delay(watering_delay);
+    digitalWrite(relay_pin, false);
   }
 }
 
 void setup() {
   Serial.begin(9600);
   BT.begin(9600);
-  
   Wire.begin();// Start the I2C interface
-  /*set_time();  第一次傳就好*/
   pinMode(relay_pin,OUTPUT);
+  /*set_time();  just upload it in the first time*/
 }
 
 void loop(){
   //show_time();
   String readin = "0";
-  int hh = Clock.getHour(h12,PM);
-  
-  if(hh == 6 || hh == 16)
+  int current_hour = Clock.getHour(h12,PM);
+
+  if(current_hour == morning || current_hour == night)
 	  water();
-	//早上5點 晚上7點澆水
-  if(hh == 7 || hh == 17)
-	  water_flag = true;
+  else if(current_hour == morning+1 || current_hour == night+1)
+	  had_been_irrigated = false;
 
   if(BT.available()){
     readin = BT.readString();
     Serial.print(readin);
     Serial.write("\n");
     digitalWrite(relay_pin, readin[0]-'0');
-    //強制開水
-  }  
+    //if readin is 1, that is open the water gate
+  }
 
 }
