87291d3ee868e89dde08099c51ceee057ab26b05
25077667 <zxc25077667@gmail.com>
2019-01-26 01:35:28 +0800

    renew all

--- main_programs/main_lib/main_lib.ino
+++ main_programs/main_lib/main_lib.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 #define relay_pin 2
+//#define watering_delay 185185
 
 DS3231 Clock;
 bool h12;
@@ -10,7 +11,7 @@ bool Century=false;
 bool had_been_irrigated = false;
 int morning = 6;
 int night = 16;
-int watering_delay = 185185;
+unsigned long watering_delay;
 SoftwareSerial BT(7, 6); //arduino RX/TX
 
 void set_time(){
@@ -23,7 +24,7 @@ void set_time(){
   Clock.setSecond(0);
 }
 
-void show_time(){
+void show_info(){
       Serial.print(Clock.getYear(), DEC);
       Serial.print("-");
       Serial.print(Clock.getMonth(Century), DEC);
@@ -35,6 +36,12 @@ void show_time(){
       Serial.print(Clock.getMinute(), DEC);
       Serial.print(":");
       Serial.println(Clock.getSecond(), DEC);
+      Serial.print(morning);
+      Serial.print("\t");
+      Serial.print(night);
+      Serial.print("\t");
+      Serial.print(watering_delay);
+      Serial.print("\n");
       delay(1000);
 }
 
@@ -47,55 +54,55 @@ void water(){
   }
 }
 
+void bluetooth_control(){
+  String readin = "0";
+  if(BT.available()){
+
+    readin = BT.readString();
+    Serial.print(readin);
+    Serial.write("\n");
+    if (readin[0]=='0')
+      return;
+    else if(readin[0]=='m')
+      morning = (readin[1]-'0')*10+(readin[2]-'0');
+
+    else if(readin[0]=='a')
+      night = (readin[1]-'0')*10+(readin[2]-'0');
+
+    else if(readin[0]=='s'){
+      int index = 1;
+      unsigned long tmp = 0;
+      while(readin[index]!='q'){
+        tmp = tmp*10 + (readin[index]-'0');
+        index++;
+      }
+      watering_delay = tmp;
+    }
+
+    else if (readin[0]=='w')
+      digitalWrite(relay_pin, readin[1]-'0');
+    else
+      Serial.print("readin failed!\n");
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   BT.begin(9600);
   Wire.begin();// Start the I2C interface
   pinMode(relay_pin,OUTPUT);
   /*set_time();  just upload it in the first time*/
-}
-
-void bluetooth_control(){
-  String readin = "0";
-  if(BT.available()){
-    readin = BT.readString();
-    Serial.print(readin);
-    Serial.write("\n");
-    if(readin[0]=='m'){
-      morning = (readin[1]-'0')*10+(readin[2]-'0');
-    }
-    else if(readin[0]=='a'){
-      night = (readin[1]-'0')*10+(readin[2]-'0');
-    }
-    else if(readin[0]=='s'){
-      int counter = 1;
-      int tmp = 0;
-      while(readin[counter]!='q'){
-        tmp = tmp*10 + (readin[counter]-'0');
-        counter++;
-      }
-      watering_delay = tmp;
-    }
-    else if (readin[0]=='w'){
-      digitalWrite(relay_pin, readin[1]-'0');
-    }
-  }
-  Serial.write(morning);
-  Serial.write("\t");
-  Serial.write(night);
-  Serial.write("\t");
-  Serial.write(watering_delay);
-  Serial.write("\n");
+  watering_delay = 370370;
 }
 
 void loop(){
-  //show_time();
+  show_info();
   int current_hour = Clock.getHour(h12,PM);
 
   if(current_hour == morning || current_hour == night)
 	  water();
   else if(current_hour == morning+1 || current_hour == night+1)
 	  had_been_irrigated = false;
-   
+
   bluetooth_control();
 }
