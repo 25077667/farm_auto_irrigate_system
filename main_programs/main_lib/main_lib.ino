fa58a19e987d154471fc690c858bad10acb9a7c2
25077667 <zxc25077667@gmail.com>
2020-01-15 22:33:24 +0800

    renew settime

--- main_programs/main_lib/main_lib.ino
+++ main_programs/main_lib/main_lib.ino
@@ -7,46 +7,46 @@
 DS3231 Clock;
 bool h12;
 bool PM;
-bool Century=false;
+bool Century = false;
 bool had_been_irrigated = false;
 int morning;
 int night;
 unsigned long watering_delay;
 SoftwareSerial BT(7, 6); //arduino RX/TX
 
-void set_time(){
-  Clock.setYear(19);
+void set_time() {
+  Clock.setYear(20);
   Clock.setMonth(1);
-  Clock.setDate(26);
+  Clock.setDate(12);
   Clock.setDoW(0);
-  Clock.setHour(11);	//the format is 24 hours
-  Clock.setMinute(14);
+  Clock.setHour(10);	//the format is 24 hours
+  Clock.setMinute(27);
   Clock.setSecond(0);
 }
 
-void show_info(){
-      Serial.print(Clock.getYear(), DEC);
-      Serial.print("-");
-      Serial.print(Clock.getMonth(Century), DEC);
-      Serial.print("-");
-      Serial.print(Clock.getDate(), DEC);
-      Serial.print(" ");
-      Serial.print((int)Clock.getHour(h12,PM), DEC); //24-hr
-      Serial.print(":");
-      Serial.print(Clock.getMinute(), DEC);
-      Serial.print(":");
-      Serial.println(Clock.getSecond(), DEC);
-      Serial.print(morning);
-      Serial.print("\t");
-      Serial.print(night);
-      Serial.print("\t");
-      Serial.print(watering_delay);
-      Serial.print("\n");
-      delay(1000);
+void show_info() {
+  Serial.print(Clock.getYear(), DEC);
+  Serial.print("-");
+  Serial.print(Clock.getMonth(Century), DEC);
+  Serial.print("-");
+  Serial.print(Clock.getDate(), DEC);
+  Serial.print(" ");
+  Serial.print((int)Clock.getHour(h12, PM), DEC); //24-hr
+  Serial.print(":");
+  Serial.print(Clock.getMinute(), DEC);
+  Serial.print(":");
+  Serial.println(Clock.getSecond(), DEC);
+  Serial.print(morning);
+  Serial.print("\t");
+  Serial.print(night);
+  Serial.print("\t");
+  Serial.print(watering_delay);
+  Serial.print("\n");
+  delay(1000);
 }
 
-void water(){
-  if ( had_been_irrigated == false ){
+void water() {
+  if ( had_been_irrigated == false ) {
     had_been_irrigated = true;
     digitalWrite(relay_pin, true);
     delay(watering_delay);
@@ -54,33 +54,33 @@ void water(){
   }
 }
 
-void bluetooth_control(){
+void bluetooth_control() {
   String readin = "0";
-  if(BT.available()){
+  if (BT.available()) {
 
     readin = BT.readString();
     Serial.print(readin);
     Serial.write("\n");
-    if (readin[0]=='0')
+    if (readin[0] == '0')
       return;
-    else if(readin[0]=='m')
-      morning = (readin[1]-'0')*10+(readin[2]-'0');
+    else if (readin[0] == 'm')
+      morning = (readin[1] - '0') * 10 + (readin[2] - '0');
 
-    else if(readin[0]=='a')
-      night = (readin[1]-'0')*10+(readin[2]-'0');
+    else if (readin[0] == 'a')
+      night = (readin[1] - '0') * 10 + (readin[2] - '0');
 
-    else if(readin[0]=='s'){
+    else if (readin[0] == 's') {
       int index = 1;
       unsigned long tmp = 0;
-      while(readin[index]!='q'){
-        tmp = tmp*10 + (readin[index]-'0');
+      while (readin[index] != 'q') {
+        tmp = tmp * 10 + (readin[index] - '0');
         index++;
       }
       watering_delay = tmp;
     }
 
-    else if (readin[0]=='w')
-      digitalWrite(relay_pin, readin[1]-'0');
+    else if (readin[0] == 'w')
+      digitalWrite(relay_pin, readin[1] - '0');
     else
       Serial.print("readin failed!\n");
   }
@@ -90,21 +90,21 @@ void setup() {
   Serial.begin(9600);
   BT.begin(9600);
   Wire.begin();// Start the I2C interface
-  pinMode(relay_pin,OUTPUT);
+  pinMode(relay_pin, OUTPUT);
   //set_time();  //just upload it in the first time
   watering_delay = 370370;
   morning = 6;
   night = 16;
 }
 
-void loop(){
+void loop() {
   show_info();
-  int current_hour = Clock.getHour(h12,PM);
+  int current_hour = Clock.getHour(h12, PM);
 
-  if(current_hour == morning || current_hour == night)
-	  water();
-  else if(current_hour == morning+1 || current_hour == night+1)
-	  had_been_irrigated = false;
+  if (current_hour == morning || current_hour == night)
+    water();
+  else if (current_hour == morning + 1 || current_hour == night + 1)
+    had_been_irrigated = false;
 
   bluetooth_control();
 }
