86084eea33964e41302bdc3f0fe6f5cc9af0001b
25077667 <zxc25077667@gmail.com>
2020-02-18 17:02:40 +0800

    Test for IO

---
+++ HMI_control/arduino/test_IO/test_IO.ino
@@ -0,0 +1,23 @@
+int isAllCorrect(){
+  //return last wrong index
+  int index = -1;
+  int counter = 0;
+  while(Serial.available()){
+    char c = Serial.read();
+    if(c != 'a')
+      index = counter;
+    counter++;
+  }
+  return index;
+}
+
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  if (Serial.available()){
+    String msg = Serial.readStringUntil('\n');
+    Serial.println(msg);
+    delay(50);
+  }
+}
